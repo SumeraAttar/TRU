04dbdfd74119e29a0838763ebe409ea0afae4745
SumeraAttar <sumeraattar2001@gmail.com>
2024-05-15 14:17:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your interest! The case study will be sent to your email.');
+});
